867331e5261f7469214cd02736bd4960bc4704c7
nicoacos7a <nicoacos7a@gmail.com>
2020-06-19 15:41:42 -0300

    3 files modified

--- src/app/components/empresas/empresas.component.ts
+++ src/app/components/empresas/empresas.component.ts
@@ -59,35 +59,47 @@ export class EmpresasComponent implements OnInit {
      this.GetEmpresas();
   }
 
-  
-
-
   GetEmpresas() {
         //  this.empresasService.get(' ', this.Pagina).subscribe((res: Empresa[]) => {
-       this.empresasService.get().subscribe((res: Empresa[]) => {
+       this.empresasService.get(' ').subscribe((res: Empresa[]) => {
        this.Lista = res;
      });
   }
 
   Agregar() {
     this.AccionABMC = "A";
+    // this.FormReg.reset({Activo: true});
+    this.FormReg.reset();
   }
 
   // Buscar segun los filtros, establecidos en FormReg
-  Buscar() {
-    //  this.empresasService.get(' ', this.Pagina).subscribe((res: any) => {
-      this.empresasService.get().subscribe((res: any) => {
-        this.Lista = res.Lista;
-        this.RegistrosTotal = res.RegistrosTotal;
-      });
-     this.SinBusquedasRealizadas = false;
-  }
+  // Buscar() {
+  //   this.empresasService
+  //     .get(' ')
+  //     .subscribe((res: any) => {
+  //       this.Lista = res.Lista;
+  //       this.RegistrosTotal = res.RegistrosTotal;
+  //     });
+  //   this.SinBusquedasRealizadas = false;
+  // }
 
   // Obtengo un registro especifico según el Id
   BuscarPorId(Dto, AccionABMC) {
     window.scroll(0, 0); // ir al incio del scroll
-    this.AccionABMC = AccionABMC;
+ 
+    this.empresasService.getById(Dto.IdEmpresa).subscribe((res: any) => {
+      // hacemos copia para no modificar el array original del mock
+      const itemCopy = { ...res }; 
+
+      //formatear fecha de  ISO 8061 a string dd/MM/yyyy
+      var arrFecha = itemCopy.FechaFundacion.substr(0, 10).split("-");
+      itemCopy.FechaFundacion = arrFecha[2] + "/" + arrFecha[1] + "/" + arrFecha[0];
+
+      this.FormReg.patchValue(itemCopy);
+      this.AccionABMC = AccionABMC;
+    });
   }
+
 
   Consultar(Dto) {
     this.BuscarPorId(Dto, "C");
@@ -100,9 +112,38 @@ export class EmpresasComponent implements OnInit {
 
   // grabar tanto altas como modificaciones
   Grabar() {
-    alert("Registro Grabado!");
-    this.Volver();
+    //hacemos una copia de los datos del formulario, para modificar la fecha y luego enviarlo al servidor
+    const itemCopy = { ...this.FormReg.value };
+ 
+    //convertir fecha de string dd/MM/yyyy a ISO para que la entienda webapi
+    var arrFecha = itemCopy.FechaFundacion.substr(0, 10).split("/");
+    if (arrFecha.length == 3)
+      itemCopy.FechaFundacion = 
+          new Date(
+            arrFecha[2],
+            arrFecha[1] - 1,
+            arrFecha[0]
+          ).toISOString();
+ 
+    // agregar post
+    if (itemCopy.IdEmpresa == 0 || itemCopy.IdEmpresa == null) {
+      this.empresasService.post(itemCopy).subscribe((res: any) => {
+        this.Volver();
+        alert('Registro agregado correctamente.');
+        //this.Buscar();
+      });
+    } else {
+      // modificar put
+      this.empresasService
+        .put(itemCopy.IdEmpresa, itemCopy)
+        .subscribe((res: any) => {
+          this.Volver();
+          alert('Registro modificado correctamente.');
+          //this.Buscar();
+        });
+    }
   }
+
 
   // Volver desde Agregar/Modificar
   Volver() {
--- src/app/services/empresas.service.ts
+++ src/app/services/empresas.service.ts
@@ -6,6 +6,7 @@ import {
   HttpParams
 } from "@angular/common/http";
 import { of } from "rxjs";
+import { Empresa } from "../models/empresa";
 
 @Injectable({
   providedIn: "root"
@@ -17,7 +18,32 @@ export class EmpresasService {
     this.resourceUrl = "https://pavii.ddns.net/api/empresas/";
   }
 
-  get() {
-    return this.httpClient.get(this.resourceUrl);
+  // get() {
+  //   return this.httpClient.get(this.resourceUrl);
+  // }
+
+  get(Nombre: string) {
+    let params = new HttpParams();
+    if (Nombre != null) {
+      params = params.append("Nombre", Nombre);
+    }
+
+    return this.httpClient.get(this.resourceUrl, { params: params });
+  }
+
+  getById(Id: number) {
+    return this.httpClient.get(this.resourceUrl + Id);
+  }
+
+  post(obj:Empresa) {
+    return this.httpClient.post(this.resourceUrl, obj);
+  }
+
+  put(Id: number, obj:Empresa) {
+    return this.httpClient.put(this.resourceUrl + Id, obj);
+  }
+
+  delete(Id) {
+    return this.httpClient.delete(this.resourceUrl + Id);
   }
 }
